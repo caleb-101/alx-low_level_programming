3ad4202a61e64e0021e693ed3844d60cde9c55a3
caleb-101 <calebkofiborlu@gmail.com>
2023-10-14 12:46:20 -0700

    updated 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+*main-this is the entry point of the function
+*
+*Return: Always 0(Sucess)
+*/
+
+#include <stdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return(0);
+}    
