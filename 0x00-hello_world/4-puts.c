6ba5c8d952506068a3e35133c3379ccb4f6061c2
caleb-101 <calebkofiborlu@gmail.com>
2023-10-14 12:03:08 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(void)
-/*using puts to print a simple string with a new line*/
+/**
+using puts to print a simple string with a new line
+*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 
