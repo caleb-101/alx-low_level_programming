8e3f31b6b176c46ddda553ffba708aef6406d1f9
Caleb Borlu <calebkofiborlu@gmail.com>
2023-10-23 21:38:11 +0000

    another down

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_strpbrk - The _strpbrk() 
+* occurrence in the string s of any of the bytes in
+*the string accept
+*
+*@s: input 
+*@accept: input 
+*
+*Return: Always 0
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+				return (s + i);
+		}
+	}
+
+	return (NULL);
+}
